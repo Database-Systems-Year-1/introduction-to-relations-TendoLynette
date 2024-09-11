70d6e4061c3efbe53c326d485ef099ad2ac30bc2
TendoLynette <167431776+TendoLynette@users.noreply.github.com>
2024-09-11 13:23:31 +0300

    Update branch tab.sql

--- branch table/branch tab.sql
+++ branch table/branch tab.sql
@@ -1,7 +1,5 @@
-Create table branch(branch_id int,primary key(branch_id),branch_name varchar(9),mgr_id int,mgr_start_date int);
-Insert into branch(branch_id, branch_name, mgr_id,mgr_start_date)
-values
-(1,'Corporate',100,2006-02-09),
-(2,'Scranton',102,1992-04-06),
-(3,'Stamford',106,1998-02-13);
-select* from branch;
+create table Branch(branch_id int,primary key(branch_id),branch_name varchar(9),mgr_id int,mgr_start_date date);
+insert into Branch values(1,'Corporate',100,'2006-02-09');
+insert into Branch values(2,'Scranton',102,'1992-04-06');
+insert into Branch values(3,'Stamford',106,'1998-02-13');
+select * from Branch;     
